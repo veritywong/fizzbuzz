a1370463bbec84211651c3eeaec24202be28ac5e
Verity <veritywong90@gmail.com>
2023-04-07 16:30:19 +0100

    test using Integer class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,12 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  else
-    number
+class Integer
+
+  def fizzbuzz
+    if self % 3 == 0 && self % 5 == 0
+     'fizzbuzz'
+    elsif self % 3 == 0
+      'fizz'
+    else self % 5 == 0
+      'buzz'
+    end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,15 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do 
-    it 'returns "fizz" when passed 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
-    end
+describe Integer do 
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq("fizz")
+  end
+
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq("buzz")
+  end
+
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq("fizzbuzz")
+  end
 end 
